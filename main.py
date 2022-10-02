efb078f26c908f10940c590dddad4213b9706fb2
KateDykiert <kasia.dykiert@gmail.com>
2022-10-02 18:42:27 +0200

    refactoring

--- main.py
+++ main.py
@@ -3,47 +3,37 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest
 import page
-from element import BasePageElement
-from locator import *
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.keys import Keys
 
 
 class GoogleSearch(unittest.TestCase):
 
     def setUp(self):
+
+        # driver and home_page declaration
         s = Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=s)
         self.driver.get('https://www.google.com')
         self.home_page = page.HomePage(self.driver)
         self.home_page.accept_cookies()
 
-    # def test_quick_language_change(self):
-    #     self.home_page.quick_language_change()
-    #     assert True
+    def test_explicit_contents(self):
 
-    def not_test_unsuccessful_sign_in(self):
+        # search phrase in the engine
+        self.home_page.search("marvel cinematic universe")
 
-        # checking if incorrect email is being handled correctly
-        assert not self.home_page.sign_in("not.a.real.kasia.dykiert.email", "")
-
-    def not_test_handling_0_results(self):
-
-        # search phrase in the engine and click enter
-        search_input = BasePageElement(self.driver, HomePageLocators.MAIN_SEARCH)
-        search_input.set_text("tyhlvcejfliguhgvjocldfghnimldi nftgliudrvhmd;odrfinr")
-        search_input.send_key(Keys.ENTER)
-
+        # check if explicit search is correctly on or off
         result_page = page.SearchResultPage(self.driver)
-        assert not result_page.any_results()
+        result_page.switch_explicit_search()
+        assert result_page.is_safe_search_on()
+        result_page.switch_explicit_search()
+        assert not result_page.is_safe_search_on()
 
-    def not_test_change_result_page(self):
+    def test_change_result_page(self):
 
-        # search phrase in the engine and click enter
-        search_input = BasePageElement(self.driver, HomePageLocators.MAIN_SEARCH)
-        search_input.set_text("Apple")
-        search_input.send_key(Keys.ENTER)
+        # search phrase in the engine
+        self.home_page.search("Apple")
 
         # check if there is more than 0 results
         result_page = page.SearchResultPage(self.driver)
@@ -66,22 +56,22 @@ class GoogleSearch(unittest.TestCase):
             print("Has not returned to the home page")
             assert False
 
-    def test_explicit_contents(self):
+    def test_handling_0_results(self):
 
-        # search phrase in the engine and click enter
-        search_input = BasePageElement(self.driver, HomePageLocators.MAIN_SEARCH)
-        search_input.set_text("marvel cinematic universe")
-        search_input.send_key(Keys.ENTER)
+        # search phrase in the engine
+        self.home_page.search("tyhlvcejfliguhgvjocldfghnimldi nftgliudrvhmd;odrfinr")
 
+        # see if 0 results is being handled properly
         result_page = page.SearchResultPage(self.driver)
-        result_page.switch_explicit_search()
-        assert result_page.is_safe_search_on()
-        result_page.switch_explicit_search()
-        assert not result_page.is_safe_search_on()
+        assert not result_page.any_results()
+
+    def test_unsuccessful_sign_in(self):
+
+        # checking if incorrect email is being handled correctly
+        assert not self.home_page.sign_in("not.a.real.kasia.dykiert.email", "")
 
     def tearDown(self):
-        # self.driver.close()
-        pass
+        self.driver.close()
 
 
 if __name__ == '__main__':
--- page.py
+++ page.py
@@ -3,6 +3,7 @@ from element import BasePageElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
 import time
 
 
@@ -11,12 +12,26 @@ class BasePage(object):
         self.driver = driver
 
     def wait_for_element(self, locator):
-        return WebDriverWait(self.driver, 100).until(
+
+        """
+        Waits for the provided WebElement to be present on the DOM of a page and visible.
+
+        :param locator: tuple of 2 elements - (selenium By, string) E.g. (By.NAME, "Passwd")
+        :return: WebElement
+        """
+        return WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located(locator)
         )
 
     def sign_in(self, login, password):
 
+        """
+        Performs full sign in from the home page with provided credentials.
+
+        :param login: string
+        :param password: string
+        :return: Boolean - True if sign in has been successful. Otherwise, returns False.
+        """
         # click "sign in" button
         sign_in = self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div/div/div/div[2]/a")
         sign_in.click()
@@ -32,6 +47,7 @@ class BasePage(object):
                     lambda driver: "Nie możemy znaleźć takiego konta Google" in driver.page_source
                 )
             return False
+            print("Provided login has been rejected.")
         except TimeoutException:
             print("Provided login has been accepted.")
 
@@ -46,6 +62,7 @@ class BasePage(object):
                     lambda driver: "Nieprawidłowe hasło. Spróbuj jeszcze raz lub kliknij „Nie pamiętasz hasła?”, "
                                "by je zresetować." in driver.page_source
                 )
+            print("Provided password has been rejected.")
             return False
         except TimeoutException:
             print("Provided password has been accepted.")
@@ -54,56 +71,112 @@ class BasePage(object):
         time.sleep(5)
         if self.driver.title == "Google":
             return True
+        print("Failed to get redirected.")
         return False
 
 
 class HomePage(BasePage):
 
     def accept_cookies(self):
+
+        """
+        Accepts cookies of Google.
+        """
         accept_all_button = self.wait_for_element(HomePageLocators.ACCEPT_ALL_COOKIES)
         accept_all_button.click()
 
-    def quick_language_change(self):
-        suggested_language = self.driver.find_element(*HomePageLocators.SUGGESTED_LANGUAGE)
-        suggested_language.click()
+    # def quick_language_change(self):
+    #     suggested_language = self.driver.find_element(*HomePageLocators.SUGGESTED_LANGUAGE)
+    #     suggested_language.click()
+
+    def search(self, phrase):
+
+        """
+        Performs a Google search of a provided phrase.
+
+        :param phrase: string
+        """
+        search_input = BasePageElement(self.driver, HomePageLocators.MAIN_SEARCH)
+        search_input.set_text(phrase)
+        search_input.send_key(Keys.ENTER)
 
 
 class SearchResultPage(BasePage):
 
     def go_to_next_page(self):
+
+        """
+        Goes to next page of Google search results.
+        """
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         self.driver.find_element(*SearchResultPageLocators.NEXT_PAGE).click()
 
     def any_results(self):
+
+        """
+        Checks if there are any results on the page.
+
+        :return: Boolean - True if there are more than 0 results on the page. False if there are no results.
+        """
+        # the below method took more time, so it's commented.
+        # try:
+        #     self.wait_for_element(SearchResultPageLocators.RESULTS)
+        # except TimeoutException:
+        #     print("There are no results.")
+        #     return False
+        # print("There is more than 0 results.")
+        # return True
+
         try:
             WebDriverWait(self.driver, 5).until(
                     lambda driver: "Około 0 wyników" in driver.page_source
                 )
+            print("There are no results.")
             return False
         except TimeoutException:
             print("There is more than 0 results.")
             return True
 
     def is_page_number_matching(self, expected_number):
+
+        """
+        Checks if page number displayed is the one corresponding to the one provided in the argument.
+
+        :param expected_number: integer
+        :return: Boolean - True if the page number is corresponding to expected_number. Otherwise, False.
+        """
         try:
             WebDriverWait(self.driver, 5).until(
                     lambda driver: "Strona {} z około".format(str(expected_number)) in driver.page_source
                 )
+            print("Correct page number is being displayed.")
             return True
         except TimeoutException:
             print("Incorrect page number is being displayed.")
             return False
 
     def go_to_home_page(self):
+
+        """
+        Clicks the logo of Google and forwards to the homepage of Google.
+        """
         self.driver.find_element(*SearchResultPageLocators.HOME_PAGE).click()
 
     def switch_explicit_search(self):
+
+        """
+        Switches the Explicit content switch.
+        """
         self.driver.find_element(*SearchResultPageLocators.SETTINGS).click()
-        WebDriverWait(self.driver, 5).until(
-            EC.visibility_of_element_located(SearchResultPageLocators.EXPLICIT_SWITCH)
-        ).click()
+        self.wait_for_element(SearchResultPageLocators.EXPLICIT_SWITCH).click()
 
     def is_safe_search_on(self):
+
+        """
+        Checks if the SafeSearch on functionality is on.
+
+        :return: Boolean - True of the SafeSearch is on. Otherwise, False.
+        """
         try:
             WebDriverWait(self.driver, 5).until(
                     lambda driver: "SafeSearch włączony" in driver.page_source
